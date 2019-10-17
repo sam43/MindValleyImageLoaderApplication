dd2f4b805a67aca466cb95a79e9c0aa07357db39
sam43 <scode43@gmail.com>
2019-10-18 02:40:53 +0600

    UI update

--- app/src/main/java/com/sam43/mindvalleyimageloaderapplication/session/SessionConstants.kt
+++ app/src/main/java/com/sam43/mindvalleyimageloaderapplication/session/SessionConstants.kt
@@ -2,18 +2,6 @@ package com.sam43.mindvalleyimageloaderapplication.session
 
 class SessionConstants {
     companion object {
-        const val USER_PHONE_NO = "PREF_USER_PHONE_NO"
-        const val PREF_AUTH = "PREF_USER_LOGIN_SIGNUP"
-        const val PREF_NAME = "PREF_PREFEEX"
-        const val PREVIEW_FOOD_ORDER = "PFM"
-        const val BOOKING_PREF = "BOOKING_PREF"
-        const val USER = "USER"
-        const val SOCIAL_LOGIN = "SOCIAL_LOGIN"
-        const val SOCIAL_LOGIN_NAME = "SOCIAL_LOGIN_NAME"
-        const val SOCIAL_LOGIN_PROVIDER = "adfadsfadsf"
-        const val BOOKING_TABLES = "BOOKING_TABLES"
-        const val AUTH_RUNNING = "ASdklkafdlkjas"
-        const val FIRST_LOGIN = "FIRST_LOGIN"
-        const val SEARCH_PREFERENCE = "SEARCH_PREFERENCE"
+        const val PREF_NAME = "PREF_APP"
     }
 }
